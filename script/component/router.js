27182483aa066cad50803b98e6c77f68cd0202f7
452082031@qq.com <452082031@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-04-14 06:53:48 +0000

    target
    git-svn-id: svn://svn4.apicloud.com/A6078888839613/%E5%9B%9E%E6%94%B6%E6%89%8B%E6%9C%BA@4 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/component/router.js
+++ script/component/router.js
@@ -1,11 +1,33 @@
 window.ROUTER = {
   'personal-center': {
-    path: 'widget://html/wode/personal-center.html'
+    path: 'widget://html/wode/personal-center.html',
+    animationDirection: 'from_left'
   },
   'personal-information': {
     path: 'widget://html/wode/personal-information.html'
   },
   'empower': {
     path: 'widget://html/shouquan/empower.html'
-  }
+  },
+  'login': {
+    path: 'widget://html/empower/login.html'
+  },
+  'register': {
+    path: 'widget://html/empower/register.html'
+  },
+  'recycling-records': {
+    path: 'widget://html/records/recycling-records.html'
+  },
+  'shipping-address': {
+    path: 'widget://html/records/shipping-address.html'
+  },
+  'yinhangka': {
+    path: 'widget://html/wode/yinhangka.html'
+  },
+  'help-center': {
+    path: 'widget://html/wode/help-center.html'
+  },
+  'yijianfankui': {
+    path: 'widget://html/wode/yijianfankui.html'
+  },
 }
