e65d77cfb0603d16f9a12dece1c6a7b450986ba6
rootHax0rs <Hax0rs@chamd5.org>
2018-04-28 10:50:59 +0800

    test

--- script/component/router.js
+++ script/component/router.js
@@ -1,86 +1,89 @@
 window.ROUTER = {
-  'personal-center': {
-    path: 'widget://html/wode/personal-center.html'
-  },
-  'personal-information': {
-    path: 'widget://html/wode/personal-information.html'
-  },
-  'login': {
-    path: 'widget://html/empower/login.html'
-  },
-  'input-password': {
-    path: 'widget://html/empower/input-password.html'
-  },
-  'register': {
-    path: 'widget://html/empower/register.html'
-  },
-  'modify-password': {
-    path: 'widget://html/empower/modify-password.html'
-  },
-  'forget-password': {
-    path: 'widget://html/empower/forget-password.html'
-  },
-  'recycling-records': {
-    path: 'widget://html/records/recycling-records.html'
-  },
-  'shipping-address': {
-    path: 'widget://html/records/shipping-address.html'
-  },
-  'add-shipping-address': {
-    path: 'widget://html/records/add-shipping-address.html'
-  },
-  'yinhangka': {
-    path: 'widget://html/wode/yinhangka.html'
-  },
-  'help-center': {
-    path: 'widget://html/wode/help-center.html'
-  },
-  'yijianfankui': {
-    path: 'widget://html/wode/yijianfankui.html'
-  },
-  'feedback': {
-    path: 'widget://html/wode/feedback.html'
-  },
-  'setting': {
-    path: 'widget://html/wode/setting.html'
-  },
-  'offer': {
-    path: 'widget://html/home/offer.html'
-  },
-  'records': {
-    path: 'widget://html/records/recycling-records.html'
-  },
-  'details':{
-    path: 'widget://html/records/records-detail.html'
-  },
-  'renzheng': {
-    path: 'widget://html/renzheng/renzheng.html'
-  },
-  'gerenxinxi': {
-    path: 'widget://html/renzheng/gerenxinxi.html'
-  },
-  'lianxiren': {
-    path: 'widget://html/renzheng/lianxiren.html'
-  },
-  'yunyingshang': {
-    path: 'widget://html/renzheng/yunyingshang.html'
-  },
-  'zhimafen': {
-    path: 'widget://html/renzheng/zhimafen.html'
-  },
-  'addcard':{
-     path: 'widget://html/records/addcard.html'
-  },
-  'submit-order':{
-    path: 'widget://html/records/submit-order.html'
-  },
-  'home':{
-    path: 'widget://html/home/home.html'
-  },
-  'refund':{
-    path: 'widget://html/wode/refund.html'
-  },
-  'relationship': {
+    'personal-center': {
+      path: 'widget://html/wode/personal-center.html'
+    },
+    'personal-information': {
+      path: 'widget://html/wode/personal-information.html'
+    },
+    'login': {
+      path: 'widget://html/empower/login.html'
+    },
+    'input-password': {
+      path: 'widget://html/empower/input-password.html'
+    },
+    'register': {
+      path: 'widget://html/empower/register.html'
+    },
+    'modify-password': {
+      path: 'widget://html/empower/modify-password.html'
+    },
+    'forget-password': {
+      path: 'widget://html/empower/forget-password.html'
+    },
+    'recycling-records': {
+      path: 'widget://html/records/recycling-records.html'
+    },
+    'shipping-address': {
+      path: 'widget://html/records/shipping-address.html'
+    },
+    'add-shipping-address': {
+      path: 'widget://html/records/add-shipping-address.html'
+    },
+    'yinhangka': {
+      path: 'widget://html/wode/yinhangka.html'
+    },
+    'help-center': {
+      path: 'widget://html/wode/help-center.html'
+    },
+    'yijianfankui': {
+      path: 'widget://html/wode/yijianfankui.html'
+    },
+    'feedback': {
+      path: 'widget://html/wode/feedback.html'
+    },
+    'setting': {
+      path: 'widget://html/wode/setting.html'
+    },
+    'offer': {
+      path: 'widget://html/home/offer.html'
+    },
+    'records': {
+      path: 'widget://html/records/recycling-records.html'
+    },
+    'details':{
+      path: 'widget://html/records/records-detail.html'
+    },
+    'renzheng': {
+      path: 'widget://html/renzheng/renzheng.html'
+    },
+    'gerenxinxi': {
+      path: 'widget://html/renzheng/gerenxinxi.html'
+    },
+    'lianxiren': {
+      path: 'widget://html/renzheng/lianxiren.html'
+    },
+    'yunyingshang': {
+      path: 'widget://html/renzheng/yunyingshang.html'
+    },
+    'zhimafen': {
+      path: 'widget://html/renzheng/zhimafen.html'
+    },
+    'add-bank-card':{
+       path: 'widget://html/wode/add-bank-card.html'
+    },
+    'submit-order':{
+      path: 'widget://html/records/submit-order.html'
+    },
+    'home':{
+      path: 'widget://html/home/home.html'
+    },
+    'refund':{
+      path: 'widget://html/wode/refund.html'
+    },
+    'recycling-schedule':{
+      path:'widget://html/records/recycling-schedule.html'
+    },
+    'relationship': {
     path: 'widget://html/selector/relationship.html'
   }
 }
